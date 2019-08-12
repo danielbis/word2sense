fc19645d12d75608a337af916eb507148e196a81
Daniel Bis <danbisoffice@gmail.com>
2019-08-12 11:17:52 +0200

    Eval and Validation methods #8
    *  Working evaluation method with SCWS dataset.
    *  Added second BiLSTM layer

--- model.py
+++ model.py
@@ -2,10 +2,13 @@ import os
 
 import time
 
+from scipy.stats import spearmanr
+import numpy as np
 from tf_records_helper import RecordPrep
 from utils import Lang, DataLoader, load_related
 from my_cosine import my_cosine, my_mask
 import tensorflow as tf
+
 tf.compat.v1.enable_eager_execution()
 tf.executing_eagerly()
 
@@ -27,27 +30,58 @@ class Encoder(tf.keras.Model):
         then separately, the hidden state output for the last time step 
         nd the cell state for the last input time step.
         """
-        self.lstm = None
+        self.lstm_layer1 = None
+        self.lstm_layer2 = None
+
+        self.initial_state_layer_1 = [tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32),
+                                      tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32),
+                                      tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32),
+                                      tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32)]
+
+        self.initial_state_layer_2 = [tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32),
+                                      tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32),
+                                      tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32),
+                                      tf.zeros((self.batch_size, self.hidden_size), dtype=tf.float32)]
+
         if self.use_cuda:
             print("Using CUDA GPU.")
             # stateful=False by default
-            self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.CuDNNLSTM(units=self.hidden_size,
-                                                  return_sequences=True,
-                                                  return_state=True), merge_mode="concat")
+            self.lstm_layer1 = tf.keras.layers.Bidirectional(tf.keras.layers.CuDNNLSTM(units=self.hidden_size,
+                                                                                       return_sequences=True,
+                                                                                       return_state=True),
+                                                                                       merge_mode="concat")
         else:
             print("CUDA GPU unavailable, using CPU.")
-            self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=self.hidden_size,
-                                             return_sequences=True,
-                                             return_state=True), merge_mode="concat")
+            self.lstm_layer1 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=self.hidden_size,
+                                                                                  return_sequences=True,
+                                                                                  return_state=True),
+                                                                                  merge_mode="concat")
+            self.lstm_layer2 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=self.hidden_size,
+                                                                                  return_sequences=True,
+                                                                                  return_state=True),
+                                                                                  merge_mode="concat")
 
-    def call(self, _input, _initial_state):
-        outputs, h_state_f, c_state_f, h_state_b, c_state_b = self.lstm(_input, initial_state=_initial_state)
+    def call(self, _input, _initial_states):
+        """
+        Call a two-layer BiLSTM
+        :param _input:
+        :param _initial_state:
+        :return: output of layer two, layer1 cell state list, layer2 cell state list
+        """
+        outputs_1, h_state_f_1, c_state_f_1, h_state_b_1, c_state_b_1 = self.lstm_layer1(_input,
+                                                                                    initial_state=_initial_states[0])
 
-        return outputs, h_state_f, c_state_f, h_state_b, c_state_b
+        outputs_2, h_state_f_2, c_state_f_2, h_state_b_2, c_state_b_2 = self.lstm_layer2(outputs_1,
+                                                                                    initial_state=_initial_states[1])
+
+        return outputs_2, [h_state_f_1, c_state_f_1, h_state_b_1, c_state_b_1],\
+            [h_state_f_2, c_state_f_2, h_state_b_2, c_state_b_2]
+
 
 def train(embedding_matrix,
           related_matrix,
           dataset,
+          validation_dataset,
           encoder_hidden_size,
           encoder_embedding_size,
           checkpoint_dir,
@@ -60,6 +94,7 @@ def train(embedding_matrix,
     :param embedding_matrix: tensorflow variable [vocab_size x encoder_embedding_size]
     :param related_matrix: tensorflow variable [on_senses_count x 128]
     :param dataset: tf.data.TFRecordDataset
+    :param validation_dataset: tf.data.TFRecordDataset
     :param encoder_hidden_size: int
     :param encoder_embedding_size: int
     :param checkpoint_dir: str, path to checkpoints
@@ -81,7 +116,7 @@ def train(embedding_matrix,
     # update to tf nightly to fix it or use TF 2.0
     hidden_cell_zero = [tf.zeros((6, 150), dtype=tf.float32), tf.zeros((6, 150), dtype=tf.float32)]
     lstm_state = hidden_cell_zero + hidden_cell_zero  # list of 4 x tf.zeros((6, 150), dtype=tf.float32)
-    for epoch in range(0,_epochs):
+    for epoch in range(0, _epochs):
         start = time.time()  # get start of the epoch
         epoch_loss = tf.contrib.eager.Variable(0, dtype=tf.float32)  # get epoch loss
 
@@ -99,9 +134,9 @@ def train(embedding_matrix,
 
             with tf.GradientTape() as tape:
                 # f = forward, b = backward
-                outputs, hidden_f, cell_f, hidden_b, cell_b = encoder(encoder_input, lstm_state)
-                lstm_state = [hidden_f, cell_f, hidden_b, cell_b]
-                print("outputs shape: ",outputs.shape)
+                outputs, layer1_state, layer2_state = encoder(encoder_input, lstm_state)
+                lstm_state = [layer1_state, layer2_state]
+                print("outputs shape: ", outputs.shape)
                 _outs = tf.transpose(outputs, perm=[1, 0, 2])
                 print("_outs shape: ", _outs.shape)
 
@@ -111,6 +146,8 @@ def train(embedding_matrix,
                     _loss = my_cosine(_outs[i], related_embs[i])
                     _loss = my_mask(related_[i], _loss, batch_size=_batch_size, max_related=128)
                     print("_loss.shape: ", _loss.shape[0])
+                    # mask function flattens the output, so shape[0] is the total number of related words
+                    # used for cosine loss calculations in that step
                     denum = _loss.shape[0]
                     if denum == 0:
                         denum += 1
@@ -135,8 +172,60 @@ def train(embedding_matrix,
             checkpoint.save(file_prefix=checkpoint_prefix)
 
         print('Epoch {} Loss {:.4f}'.format(epoch + 1,
-                                                epoch_loss))  # could divide epoch loss by  number of batches
+                                            epoch_loss))  # could divide epoch loss by  number of batches
         print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
 
+        if epoch % 2:
+            rho, pvalue = validation(encoder, embedding_matrix=embedding_matrix, dataset=validation_dataset)
+            print("Spearman's rank correlation after Epoch {} is {:.4f}".format(epoch + 1, rho))
 
 
+def validation(encoder, embedding_matrix, dataset):
+    """
+    :param encoder: tf.keras.Model, callable
+    :param embedding_matrix: matrix storing word embeddings
+    :param dataset: tf.data.TFRecordDataset with eval or test data
+    :return: rho: Spearman's rank correlation coefficient between cosine distance and human similarity ratings from scws,
+             pvalue: the two-sided p-value for a hypothesis test whose null hypothesis
+                is that two sets of data are uncorrelated, has same dimension as rho.
+    """
+    eval_cosines = []
+    eval_ratings = []
+    num_batches = 0
+    for (batch, example) in enumerate(dataset):
+        batch_cosines = []
+        batch_ratings = []
+        # get word embeddings
+        encoder_input_1 = tf.nn.embedding_lookup(embedding_matrix, example['sentence1'])
+        encoder_input_2 = tf.nn.embedding_lookup(embedding_matrix, example['sentence2'])
+
+        sentence1_out, hidden_f, cell_f, hidden_b, cell_b = encoder(encoder_input_1, None)  # None for initial state
+        sentence2_out, hidden_f, cell_f, hidden_b, cell_b = encoder(encoder_input_2, None)
+
+        if sentence1_out.shape[0] != sentence2_out.shape[0]:
+            raise ValueError(
+                'batch_size of sentence1 and batch_size of sentence2 are not equal : '
+                'batch_size_1={}, batch_size_2={}'.format(sentence1_out.shape[0], sentence2_out.shape[0]))
+
+        for i in range(0, sentence1_out.shape[0]):
+            """
+            my_cosine
+            _input [batch_size x emb_size]
+            _related [batch_size x num_related x emb_size]
+            return: cosine distance [batch_size x num_related]
+            """
+            word1 = sentence1_out[0][example["word1idx"][i]]
+            word2 = sentence2_out[0][example["word2idx"][i]]
+
+            loss = my_cosine(tf.expand_dims(word1, 0), tf.expand_dims(tf.expand_dims(word2, 0), 0))
+            batch_cosines.append(tf.squeeze(loss))
+            batch_ratings.append(example["avg_rating"][i])
+
+        eval_cosines += batch_cosines
+        eval_ratings += batch_ratings
+        num_batches = batch
+
+    print(np.sum(eval_cosines) / num_batches)
+    rho, pvalue = spearmanr(eval_cosines, eval_ratings)
+
+    return rho, pvalue
